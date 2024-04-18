9cd7ab084511817d3d79025293c0689a4f0a0706
thao <thaont124@outlook.com.vn>
2024-04-18 20:34:04 +0700

    file

--- src/main/java/com/g11/schedule/ScheduleApplication.java
+++ src/main/java/com/g11/schedule/ScheduleApplication.java
@@ -1,9 +1,12 @@
 package com.g11.schedule;
 
+import com.g11.schedule.properties.FileProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(FileProperties.class)
 public class ScheduleApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/g11/schedule/entity/Account.java
+++ src/main/java/com/g11/schedule/entity/Account.java
@@ -14,4 +14,5 @@ public class Account {
     private int idUser;
     private String username;
     private String password;
+    private String avatar;
 }
