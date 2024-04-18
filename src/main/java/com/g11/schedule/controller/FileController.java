d48661468df5254505fae9412c6f1c25b1dfb6c4
thao <thaont124@outlook.com.vn>
2024-04-18 21:22:36 +0700

    fileURL

--- src/main/java/com/g11/schedule/controller/FileController.java
+++ src/main/java/com/g11/schedule/controller/FileController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/apiv1")
+@RequestMapping("")
 public class FileController {
     @Autowired
     private FileService fileService;
